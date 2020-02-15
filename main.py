582d50900ff33a9203c4aaf91e974178092bd1a2
jek0025 <jek0025@mix.wvu.edu>
2020-02-15 01:29:09 -0500

    handshake phase completion

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from os import path
 from Crypto.PublicKey import RSA
 import hashlib
 import string
+import random
 
 #Entry point for the program
 def main():
@@ -12,7 +13,11 @@ def main():
 	caID = 0;
 	serverID = 0;
 	serverPubKey = 0;
+	dhPrivKey = 0;
 	hashesMatch = True;
+
+	#init random number generator
+	random.seed();
 
 	#Validate that all the files exist
 	if(not validateFilesExist()):
@@ -24,6 +29,12 @@ def main():
 
 	if(not hashesMatch):
 		print('The certificate is invalid. Connection terminated.');
+
+	#simulating connection to port 443
+	logln("Client: The connection is complete");
+
+	#diffie-helmans key that both the server and the client have. We will only use this instance.
+	dhPrivKey = handshake(serverID);
 
 	print("Success!");
 	return True;
@@ -59,6 +70,66 @@ def getCAInfo():
 	logln("Client: hash calculated = " + caHash);
 
 	return (caID, serverID, serverPubKey, caHash == caPlainText[len(caPlainText) - 1]);
+
+#perform the handshake phase
+def handshake(serverID):
+	dhPrivKey = 0;
+	p = 0;	#mod p
+	g = 0;  #base g
+	a = random.randint(1,10**4);  #client private key
+	b = random.randint(1,10**4);  #server private key
+	c_x = 0;  #client generated public key
+	s_x = 0;  #server recieved client public key
+	c_y = 0;  #client recieved server public key
+	s_y = 0;  #server generated public key
+
+	logln("Client Hello: TLS version 1.3: TLS_AES_256_GCM_SHA384,TLS_CHACHA202_POLY1305_SHA256,TLS_AES_128_GCM_SHA256");
+
+	#########CLIENT CALCULATIONS################
+	with open(CONST.DHFILE,'rb') as f:
+		gp = f.read().split("\n");
+		g = int(gp[0][1:len(gp[0]) - 2]); #get g by stripping off the {}
+		p = int(gp[1][1:len(gp[1]) - 1]); #get p by stripping off the []
+
+	c_x = g**a % p;
+
+	logln("Client: Generated x - " + str(c_x));
+
+	#write client public key to file
+	with open(CONST.C2SFILE,'w') as f:
+		f.write(str(c_x));
+
+	#########SERVER CALCULATIONS################
+	#get g and p from DH... we already have them so we will use them.
+
+	s_y = g**b % p;
+
+	logln("Server: Generated y - " + str(s_y));
+
+	#write server public key to file
+	with open(CONST.S2CFILE,'w') as f:
+		f.write(str(s_y));
+
+	#read the client public key
+	with open(CONST.C2SFILE,'rb') as f:
+		s_x = int(f.read());
+
+	#use key to get dhPrivKey
+	dhPrivKey = s_x**b % p;
+
+	logln("Server: Generated secret key - " + str(dhPrivKey));
+
+	#########CLIENT CALCULATIONS###############
+
+	#read the server public key
+	with open(CONST.S2CFILE,'rb') as f:
+		c_y = int(f.read());
+
+	dhPrivKey = c_y**a % p;
+
+	logln("Client: Generated secret key - " + str(dhPrivKey));
+
+	return dhPrivKey;
 
 #Encrypt a given file (plainTextFileName) with a given RSA Public Key (pubRsaFileName)
 def rsaEncrypt(plainTextFileName,pubRsaFileName):
@@ -122,9 +193,14 @@ def validateFilesExist():
 		print("No Data File!");
 		ret = False;
 
-	#at the end, delete the old log file if it exists
+	#at the end, delete the old output files if they exists
 	if(path.exists(CONST.LOGFILE)):
 		os.remove(CONST.LOGFILE);
+	if(path.exists(CONST.C2SFILE)):
+		os.remove(CONST.C2SFILE);
+	if(path.exists(CONST.S2CFILE)):
+		os.remove(CONST.S2CFILE);
+
 	return ret;
 
 #Sets entry point for the program to run main()
